9048687d91220af59ed35b55bcf726f17af794ca
emanjoh1 <112668324+emanjoh1@users.noreply.github.com>
2024-07-08 14:32:32 -0400

    Update main.tf for pipeline

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
 module "aws-nuke" {
   source                         = "./modules"
-  cloudwatch_schedule_expression = "cron(0 00 ? * FRI *)"
-  older_than                     = "0d"
-  aws_regions                    = ["us-west-2"]
-  include_resources              = "ec2"
-  exclude_resources              = null
-  required_tags                  = "env=dev"
 }
